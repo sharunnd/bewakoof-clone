0df4087dd4070bd4eb3d2a703d6659f25a94edcb
Your Name <sharunnd11@gmail.com>
2023-04-02 17:50:29 +0530

    day-5

--- towering-grade-7940/src/routes/Navbar.jsx
+++ towering-grade-7940/src/routes/Navbar.jsx
@@ -12,7 +12,7 @@ import { AuthContext } from "../contex/AuthContestProvider";
 
 const links = [
     
-    {path:"/",text:<Image ml={120}  w={120} src={logo} alt='Dan Abramov' />},
+    {path:"/",text:<Image ml={{ base: '24px', md: '50px', lg: '120px' }}  w={{ base: '80px', md: '100px', lg: '120px' }} src={logo} alt='Dan Abramov' />},
     {path:"/men",text:"MEN"},
     {path:"/women",text:"WOWEN"},
     {path:"/mobile-covers",text:"MOBILE COVERS"}
@@ -26,46 +26,46 @@ function Navbar() {
   const handleSearch=(e)=>{
     e.preventDefault()
     setSearchItem(query)
-  }
 
+  }
     return(
         <Box className = "navbar2" borderBottom="1px solid gray" >
             <Flex>
    
             {
                 links.map((link)=>(
-                    <Box pt={2} ml={50}>
+                    <Box pt={{ base: '1px', md: '2px', lg: '2px' }} ml={{ base: '10px', md: '20px', lg: '50px' }} fontSize={{ base: '10px', md: '12px', lg: '16px' }}>
                        <NavLink key={link.path} to={link.path}>{link.text}</NavLink>
                     </Box>
 
                 ))
             }
             <Flex>
-            <Box ml={100}>
+            <Box ml={{ base: '24px', md: '40px', lg: '100px' }}>
         
             <FormControl>
                <InputGroup bg="gray.100">
-               <InputLeftElement children={<IconButton icon={<SearchIcon />} />}/>
-               <Input placeholder='Search by products'  w={250} p="auto" onChange={(e)=>setQuery(e.target.value)}/>
+               <InputLeftElement children={<IconButton  fontSize={{ base: '10px', md: '15px', lg: '15px' }} onClick={handleSearch} icon={<SearchIcon />} />}/>
+               <Input placeholder='Search by products'  w={{ base: '100px', md: '200px', lg: '250px' }}  onChange={(e)=>setQuery(e.target.value)}/>
                </InputGroup> 
             </FormControl>
             </Box>
-              <Divider orientation='vertical' ml={5} />
+             
             </Flex>
-               <Box ml={50}>
+               <Box ml={{ base: '10px', md: '30px', lg: '50px' }}>
             <Popover trigger="hover">
-              <PopoverTrigger w='30px'>
-                <IconButton bg="white" fontSize='25px' icon={<AiOutlineUser />}/>
+              <PopoverTrigger w={{ base: '10px', md: '20px', lg: '30px' }}>
+                <IconButton size={{ base: '5px', md: '10px', lg: '10px' }} bg="white" fontSize={{ base: '10px', md: '20px', lg: '20px' }} ml={{ base: '5px', md: '10px', lg: '10px' }} icon={<AiOutlineUser />}/>
 
                 </PopoverTrigger>
-                <PopoverContent w={100} bg='white' color='black'>    
+                <PopoverContent w={{ base: '60px', md: '80px', lg: '100px' }} bg='white' color='black'>    
                 <PopoverBody>
                    <NavLink to="/login">LOGIN</NavLink>
                 </PopoverBody>
                 </PopoverContent>
             </Popover>
-              <NavLink to="/wishlist"><IconButton bg="white" fontSize='25px' icon={<AiOutlineHeart />}/></NavLink>
-              <NavLink to="/bag"><IconButton bg="white" fontSize='20px' icon={<BsBag />}/></NavLink>
+              <NavLink to="/wishlist"><IconButton size={{ base: '5px', md: '10px', lg: '10px' }} ml={{ base: '5px', md: '10px', lg: '10px' }} bg="white" fontSize={{ base: '10px', md: '20px', lg: '20px' }} icon={<AiOutlineHeart />}/></NavLink>
+              <NavLink to="/bag"><IconButton bg="white" size={{ base: '5px', md: '10px', lg: '10px' }} ml={{ base: '5px', md: '10px', lg: '10px' }} fontSize={{ base: '10px', md: '20px', lg: '20px' }} icon={<BsBag />}/></NavLink>
 
             </Box>
            
